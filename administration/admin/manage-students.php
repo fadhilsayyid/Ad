8b248c08d9da3eafdaadcaf55dcd8f63907f369b
Fadhil <fadhilsayyid01@gmail.com>
2021-02-14 22:15:06 +0800

    Third Commit

--- administration/admin/manage-students.php
+++ administration/admin/manage-students.php
@@ -32,7 +32,7 @@ if(isset($_GET['del']))
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Admin | Course</title>
+    <title>Admin | Manage Students</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
