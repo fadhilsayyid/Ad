fed1555b8cbaba80c90cba25aa7073bf3cc72c44
Fadhil <fadhilsayyid01@gmail.com>
2021-02-17 20:29:48 +0800

    herokuDB

--- administration/includes/config.php
+++ administration/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','onlinecourse');
+define('DB_SERVER','us-cdbr-east-03.cleardb.com');
+define('DB_USER','bae772058f461c');
+define('DB_PASS' ,'7796a4b3');
+define('DB_NAME','heroku_b3d8476f8671abf');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
